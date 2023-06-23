4864f8571eff638be72ed0e6acfc7e3c3fab74d2
Arpit Jaswal <iamarpitjaswal@gmail.com>
2023-06-23 18:08:24 +0530

    finished basic version of this app

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = "";
 const apiURL = "https://api.openai.com/v1/chat/completions";
 
 async function postData() {
+    document.getElementById("chat-area").innerHTML="";
   const value = document.getElementById('message').value;
   console.log(value);
   const response = await fetch(apiURL, {
@@ -15,14 +16,15 @@ async function postData() {
       "messages": [
         {
           "role": "user",
-          "content": `Write a JavaScript solution code for this question and try to cover all edge cases that might arise. The question is: ${value}`
+          "content": `Write a JavaScript solution code for this question and try to cover all edge cases that might arise. And reply with "Enter a valid question, please" if you don't find any coding problem. The question is: ${value}`
         }
       ]
     })
   });
 
   const data = await response.json();
-  const res = data.choices[0].messages.content.trim();
+  console.log(data);
+  const res = data.choices[0].message.content;
   const card = document.createElement("pre");
   card.innerHTML = res;
   document.getElementById("chat-area").appendChild(card);
