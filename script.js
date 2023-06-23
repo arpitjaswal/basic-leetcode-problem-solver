9724b2672adf37ab8afffc1ae5213165d492b3d8
Arpit Jaswal <iamarpitjaswal@gmail.com>
2023-06-23 16:10:52 +0530

    finished api integration

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-const apiKey = "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";
-const apiURL = "https://api.openai.com/v1/chat/completitions"
+const apiKey = "";
+const apiURL = "https://api.openai.com/v1/chat/completions";
+
+async function postData() {
+  const value = document.getElementById('message').value;
+  console.log(value);
+  const response = await fetch(apiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {
+          "role": "user",
+          "content": `Write a JavaScript solution code for this question and try to cover all edge cases that might arise. The question is: ${value}`
+        }
+      ]
+    })
+  });
+
+  const data = await response.json();
+  const res = data.choices[0].messages.content.trim();
+  const card = document.createElement("pre");
+  card.innerHTML = res;
+  document.getElementById("chat-area").appendChild(card);
+
+}
